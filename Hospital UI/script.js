16d1ad8fa52a070864a98b43a5ee92814d23b07d
vartikaasingh <vartikasingh172@gmail.com>
2026-05-16 04:52:46 +0530

    Add files via upload

---
+++ Hospital UI/script.js
@@ -0,0 +1,11 @@
+function bookAppointment(){
+  alert("✅ Appointment Booked Successfully!");
+}
+
+function darkMode(){
+  document.body.classList.toggle("dark");
+}
+
+function appointment(){
+  alert("✅ Appointment Submitted Successfully!");
+}
